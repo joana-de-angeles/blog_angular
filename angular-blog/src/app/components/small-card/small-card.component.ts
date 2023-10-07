a55ace36a2f24c8ae02458213fbd519dd3214863
Joana de Angeles <Joana de Angeles>
2023-10-07 07:56:36 -0300

    implementação da página de artigos e responsividade

--- angular-blog/src/app/components/small-card/small-card.component.ts
+++ angular-blog/src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
 
+  @Input()
+  imgCover:string = ''
+  @Input()
+  cardTitle:string = ''
   constructor() { }
+  @Input()
+  Id: string = '0'
 
   ngOnInit(): void {
   }
